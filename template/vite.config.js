678afbc0b695c6e9b5b7e1b341c3909fe765151e
Balakrishnayempati <baluyempati007@gmail.com>
2025-04-28 19:12:48 +0530

    React_static-page

--- template/vite.config.js
+++ template/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react_static_page/',  // Ensure this matches your GitHub repository name
 })
